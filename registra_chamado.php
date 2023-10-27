f7f06ec843af339a7bb456138bf6535005382400
dougburnos <dougbrunosk@gmail.com>
2023-10-27 10:36:38 -0300

    final

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,18 +1,14 @@
 <?php
 
-    echo '<pre>'; 
-    print_r($_POST);
-    echo '</pre>'; 
-
-    
+    session_start();
 
     $titulo = str_replace('#', '-', $_POST['titulo']);
     $categoria = str_replace('#', '-', $_POST['categoria']);
     $descricao = str_replace('#', '-', $_POST['descricao']);
 
-    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+    $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
-    $arquivo = fopen('arquivo.txt', 'a');
+    $arquivo = fopen('../../app_help_desk/arquivo.txt', 'a');
 
     fwrite($arquivo, $texto);
 
--- valida_login.php
+++ valida_login.php
@@ -1,51 +1,5 @@
 <?php
 
-    session_start();
+require "../../app_help_desk/valida_login.php";
 
-    //verifica se autenticado
-    $autenticado = false;
-
-    //usuarios do sistema
-    $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => '123'),
-    );
-
-    foreach($usuarios_app as $user) {
-
-        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
-            $autenticado = true;
-        }
-
-    }
-
-    echo '<br>';
-
-    if($autenticado){
-        echo 'Usuário autenticado';
-        $_SESSION['autenticado'] = 'SIM';
-        $_SESSION['x'] = 'valor';
-        header('Location: home.php');
-    } else {
-        $_SESSION['autenticado'] = 'NÃO';
-        header('Location: index.php?login=erro');
-    }
-
-
-    /*
-    print_r($_GET);
-    echo '<br>';
-    echo $_GET['email'];
-    echo '<br>';
-    echo $_GET['senha'];    
-    */
-
-     /*
-    print_r($_POST);
-
-    echo '<br>';
-    echo $_POST['email'];
-    echo '<br>';
-    echo $_POST['senha'];
-    */  
 ?>
